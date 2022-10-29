7c9edfcb1b9eed0aa2dfaf7e60e12052aafa9428
Matthew Simmons <simmonmt@acm.org>
2022-10-29 13:02:39 -0400

    used common merged coords read library

--- cmd/calc/calc_main.cc
+++ cmd/calc/calc_main.cc
@@ -12,15 +12,14 @@
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/numbers.h"
 #include "absl/strings/str_format.h"
-#include "absl/strings/str_join.h"
-#include "absl/strings/str_split.h"
 #include "cmd/calc/calc.h"
+#include "lib/file/merged_coords.h"
 
-ABSL_FLAG(std::string, input_coords, "",
-          "File containing input coordinates. Each line is 'pixelnum x,y x,y "
-          "...'. If no value is available, use - instead of x,y.");
+ABSL_FLAG(std::string, merged_coords, "",
+          "File containing merged input coordinates. Each line is "
+          "'pixelnum x,y x,y ...'. If no value is available, use - instead "
+          "of x,y.");
 ABSL_FLAG(double, camera_distance, -1,
           "Distance of the camera to the center of the object. Units are "
           "irrelevant.");
@@ -35,75 +34,6 @@ ABSL_FLAG(int, camera_2_y_offset, 0,
           "misalignment.");
 
 namespace {
-
-struct InputRecord {
-  int pixel_num;
-  std::vector<std::optional<std::tuple<int, int>>> coords;
-};
-
-std::ostream& operator<<(std::ostream& os, const InputRecord& rec) {
-  std::vector<std::string> coords;
-  for (const auto& coord : rec.coords) {
-    if (coord.has_value()) {
-      coords.push_back(
-          absl::StrFormat("%d,%d", std::get<0>(*coord), std::get<1>(*coord)));
-    } else {
-      coords.push_back("-");
-    }
-  }
-
-  return os << absl::StrFormat("%03d %s", rec.pixel_num,
-                               absl::StrJoin(coords, " "));
-}
-
-bool ParseCoord(const std::string& str, int* x, int* y) {
-  std::vector<std::string> parts = absl::StrSplit(str, absl::MaxSplits(',', 1));
-
-  return absl::SimpleAtoi(parts[0], x) && absl::SimpleAtoi(parts[1], y);
-}
-
-absl::StatusOr<std::vector<InputRecord>> ReadInput(const std::string& path) {
-  std::ifstream infile(path);
-
-  auto error_message = [](int lineno, const std::string& suffix) {
-    return absl::StrCat("line ", lineno, ": ", suffix);
-  };
-
-  std::vector<InputRecord> recs;
-  std::string line;
-  for (int i = 1; std::getline(infile, line); ++i) {
-    std::vector<std::string> parts = absl::StrSplit(line, " ");
-    if (parts.size() != 3) {
-      return absl::InvalidArgumentError(error_message(i, "too many coords"));
-    }
-
-    unsigned int pixel_num;
-    if (!absl::SimpleAtoi(parts[0], &pixel_num) && pixel_num > 65536) {
-      return absl::InvalidArgumentError(
-          error_message(i, "pixel number invalid"));
-    }
-
-    InputRecord rec;
-    rec.pixel_num = pixel_num;
-
-    for (int j = 1; j < parts.size(); ++j) {
-      const std::string& coord = parts[j];
-      int x, y;
-      if (coord == "-") {
-        rec.coords.push_back(std::nullopt);
-      } else if (ParseCoord(coord, &x, &y)) {
-        rec.coords.emplace_back(std::make_tuple(x, y));
-      } else {
-        return absl::InvalidArgumentError(
-            error_message(i, absl::StrFormat("coordinate %d invalid", j)));
-      }
-    }
-
-    recs.push_back(rec);
-  }
-
-  return recs;
-}
 
 absl::Status WritePCD(const std::vector<XYZPos>& points,
                       const std::string& path) {
@@ -154,10 +84,10 @@ int main(int argc, char** argv) {
   QCHECK_GT(absl::GetFlag(FLAGS_resolution_v), 0)
       << "--resolution_v is required";
 
-  QCHECK(!absl::GetFlag(FLAGS_input_coords).empty())
-      << "--input_coords is required";
-  const std::vector<InputRecord> input = [&]() {
-    auto status = ReadInput(absl::GetFlag(FLAGS_input_coords));
+  QCHECK(!absl::GetFlag(FLAGS_merged_coords).empty())
+      << "--merged_coords is required";
+  const std::vector<MergedCoordsRecord> input = [&]() {
+    auto status = ReadMergedCoords(absl::GetFlag(FLAGS_merged_coords));
     QCHECK_OK(status);
     return *status;
   }();
@@ -167,7 +97,7 @@ int main(int argc, char** argv) {
   std::vector<double> pixel_y_errors;
   std::vector<XYZPos> locations;
   int num_none = 0, num_one_only = 0, num_two_only = 0;
-  for (const InputRecord& rec : input) {
+  for (const MergedCoordsRecord& rec : input) {
     LOG_IF(INFO, verbose) << rec;
 
     QCHECK_EQ(rec.coords.size(), 2);
