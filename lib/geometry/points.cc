92529d20270460f16be9b6e583063239304136dc
Matthew Simmons <simmonmt@acm.org>
2022-10-29 12:12:33 -0400

    add StrFormat printers for xyzpos and xypos

--- lib/geometry/points.cc
+++ lib/geometry/points.cc
@@ -1,14 +1,39 @@
 #include "lib/geometry/points.h"
 
-#include "absl/log/log.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 #include "absl/types/span.h"
 
 std::ostream& operator<<(std::ostream& os, const XYPos& pos) {
-  return os << "[" << pos.x << "," << pos.y << "]";
+  return os << pos.x << "," << pos.y;
 }
 
 std::ostream& operator<<(std::ostream& os, const XYZPos& pos) {
-  return os << "[" << pos.x << "," << pos.y << "," << pos.z << "]";
+  return os << pos.x << "," << pos.y << "," << pos.z;
+}
+
+absl::FormatConvertResult<absl::FormatConversionCharSet::kString |
+                          absl::FormatConversionCharSet::kIntegral>
+AbslFormatConvert(const XYPos& pos, const absl::FormatConversionSpec& spec,
+                  absl::FormatSink* s) {
+  if (spec.conversion_char() == absl::FormatConversionChar::s) {
+    s->Append(absl::StrCat("x=", pos.x, ",y=", pos.y));
+  } else {
+    s->Append(absl::StrCat("", pos.x, ",", pos.y));
+  }
+  return {true};
+}
+
+absl::FormatConvertResult<absl::FormatConversionCharSet::kString |
+                          absl::FormatConversionCharSet::kIntegral>
+AbslFormatConvert(const XYZPos& pos, const absl::FormatConversionSpec& spec,
+                  absl::FormatSink* s) {
+  if (spec.conversion_char() == absl::FormatConversionChar::s) {
+    s->Append(absl::StrCat("x=", pos.x, ",y=", pos.y, ",z=", pos.z));
+  } else {
+    s->Append(absl::StrCat("", pos.x, ",", pos.y, ",", pos.z));
+  }
+  return {true};
 }
 
 void PointsIndex::Add(const XYZPos& pos, double key) {
--- lib/geometry/points.h
+++ lib/geometry/points.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 
+#include "absl/strings/str_format.h"
 #include "absl/types/span.h"
 
 struct XYPos {
@@ -19,6 +20,17 @@ struct XYZPos {
 };
 
 std::ostream& operator<<(std::ostream& os, const XYPos& pos);
+std::ostream& operator<<(std::ostream& os, const XYZPos& pos);
+
+absl::FormatConvertResult<absl::FormatConversionCharSet::kString |
+                          absl::FormatConversionCharSet::kIntegral>
+AbslFormatConvert(const XYPos& pos, const absl::FormatConversionSpec& spec,
+                  absl::FormatSink* s);
+
+absl::FormatConvertResult<absl::FormatConversionCharSet::kString |
+                          absl::FormatConversionCharSet::kIntegral>
+AbslFormatConvert(const XYZPos& pos, const absl::FormatConversionSpec& spec,
+                  absl::FormatSink* s);
 
 class PointsIndex {
  public:
@@ -32,7 +44,5 @@ class PointsIndex {
  private:
   std::multimap<double, XYZPos> index_;
 };
-
-std::ostream& operator<<(std::ostream& os, const XYZPos& pos);
 
 #endif  // _LIB_GEOMETRY_POINTS_H_
