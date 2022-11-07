de1a7cf31fa0da4db2615f6fd46520002dfbfd09
Matthew Simmons <simmonmt@acm.org>
2022-11-07 18:39:42 -0500

    add another variant of the proto reader

--- lib/file/proto.h
+++ lib/file/proto.h
@@ -2,10 +2,21 @@
 #define _LIB_FILE_PROTO_H_ 1
 
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "google/protobuf/message.h"
 
 absl::Status ReadProto(const std::string& path,
                        google::protobuf::Message* message);
+
+template <class T>
+absl::StatusOr<T> ReadProto(const std::string& path) {
+  T pb;
+  if (absl::Status status = ReadProto(path, &pb); !status.ok()) {
+    return status;
+  }
+  return pb;
+}
+
 absl::Status WriteTextProto(const std::string& path,
                             const google::protobuf::Message& message);
 
